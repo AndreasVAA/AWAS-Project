3914cdae56daaa9b126bb80c82e9a274de13cf2a
AndreasVAA <avaa975@gmail.com>
2025-04-23 08:21:28 +0200

    FCNN with lr  0.01 and validation

--- FasterR_CNN/Faster_R_CNN_Validation_with_forwardPass_and_NMS.py
+++ FasterR_CNN/Faster_R_CNN_Validation_with_forwardPass_and_NMS.py
@@ -231,10 +231,10 @@ def validate_with_timing(weights_path, images_dir, labels_dir, output_dir,
 
 if __name__=='__main__':
     validate_with_timing(
-        weights_path='/home/itk/Desktop/Andreas/AWAS-Project/FasterR_CNN/runs/Relevant_MoreMetrics_testing_agumentations_YOLO_MosaicReducesWithEpochs_rand/best_model_absolute.pth',
+        weights_path='/home/itk/Desktop/Andreas/AWAS-Project/FasterR_CNN/runs/Testing_new_learning_rate_set_to_0.01/best_model_absolute.pth',
         images_dir='/home/itk/Desktop/Andreas/AWAS-Project/AFTI_PMID_SINGLE_CLASS_TESTING_backup_20250215_134318/val/images',
         labels_dir='/home/itk/Desktop/Andreas/AWAS-Project/AFTI_PMID_SINGLE_CLASS_TESTING_backup_20250215_134318/val/labels_minmax',
-        output_dir='Validation_FasterRCNN_with_forwardPass_and_NMS',
+        output_dir='Validation_FasterRCNN__lr_set_0.01_with_forwardPass_and_NMS',
         device='cuda',
         confidence_threshold=0.4,
         num_classes=2
